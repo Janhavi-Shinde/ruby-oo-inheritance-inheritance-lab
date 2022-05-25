70517667fe7b7346a23a2f8d6420f24d9bce8e69
janhavi <janashinde394@gmail.com>
2022-05-25 17:35:21 +1000

    all tests passing

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+         @knowledge = []
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
